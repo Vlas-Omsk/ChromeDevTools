0a184cf763d6109833d2052130999742b761bb60
Vlas <vlasdergaev@gmail.com>
2025-05-18 19:39:09 +0500

    Redirect chrome output to logger

--- source/MasterDevs.ChromeDevTools.Local/LocalChromeProcess.cs
+++ source/MasterDevs.ChromeDevTools.Local/LocalChromeProcess.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using MasterDevs.ChromeDevTools.Remote;
+using Microsoft.Extensions.Logging;
 
 namespace MasterDevs.ChromeDevTools.Local
 {
@@ -13,18 +14,21 @@ namespace MasterDevs.ChromeDevTools.Local
         private readonly string _chromePath;
         private readonly ChromeProcessParameters _parameters;
         private readonly RemoteChromeProcessFactory _remoteChromeProcessFactory;
+        private readonly ILogger<LocalChromeProcess> _logger;
         private Process? _process;
         private IChromeProcess? _remoteChromeProcess;
 
         public LocalChromeProcess(
             string chromePath,
             ChromeProcessParameters parameters,
-            RemoteChromeProcessFactory remoteChromeProcessFactory
+            RemoteChromeProcessFactory remoteChromeProcessFactory,
+            ILogger<LocalChromeProcess> logger
         )
         {
             _chromePath = chromePath;
             _parameters = parameters;
             _remoteChromeProcessFactory = remoteChromeProcessFactory;
+            _logger = logger;
         }
 
         public Task<IEnumerable<IChromeSessionConnector>> GetSessionsAsync()
@@ -48,12 +52,27 @@ namespace MasterDevs.ChromeDevTools.Local
             _process = new Process()
             {
                 StartInfo = new ProcessStartInfo(_chromePath, _parameters.Arguments)
+                {
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true
+                }
             };
+
+            _process.OutputDataReceived += DataReceived;
+            _process.ErrorDataReceived += DataReceived;
 
             if (!_process.Start())
                 throw new Exception("Process exited with code " + _process.ExitCode);
 
+            _process.BeginOutputReadLine();
+            _process.BeginErrorReadLine();
+
             _remoteChromeProcess = _remoteChromeProcessFactory.Create(new Uri("http://localhost:" + _parameters.Port));
+        }
+
+        private void DataReceived(object sender, DataReceivedEventArgs e)
+        {
+            _logger.LogDebug(e.Data);
         }
 
         private void Close()
